c3be35bfe7d80f58736bf1bd79c6e1c1efe521cc
Adryel Almeida <adryel7@gmail.com>
2025-08-14 15:12:54 -0300

    adicionando a API geradora de conslhos.

---
+++ src/js/gerador_conselho.js
@@ -0,0 +1,26 @@
+async function gerarConselho(){
+    try{
+        const url  = "https://api.adviceslip.com/advice"
+        const resposta = await fetch(url)
+        return await resposta.json()
+    }catch(error){
+        console.log("Error! Não foi possivel encontrar um conselho")
+    }
+
+}
+
+async function conselho() {
+    const json = await gerarConselho()
+    texto = json.slip.advice
+    id = json.slip.id
+    document.getElementById("titulo").innerText = `Advice #${id}`
+    document.getElementById("conselho").innerText = texto
+}
+
+document
+    .getElementById("gerador")
+    .addEventListener("click", ()=> {
+        conselho()
+    })
+
+conselho()
